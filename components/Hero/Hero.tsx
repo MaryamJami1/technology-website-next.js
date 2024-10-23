f9e7ef08785907d4eb83af4947eaf2d9072660e1
MaryamJami1 <maryamchouhan373@gmail.com>
2024-10-23 23:18:02 +0500

    hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import styles from "./hero.module.css"
+import Btn from '../Button/Btn'
 export default function hero() {
   return (
-    <div>
-     
-    </div>
+    <header>
+      <div className={styles.bg}>
+<div className="text">
+  <h1></h1>
+  <p></p>
+  <Btn text={'Get Started Now'}/>
+</div>
+<div className="img">
+
+</div>
+      </div>
+    </header>
   )
 }
