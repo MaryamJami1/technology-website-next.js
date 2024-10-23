1b7af48c0b41462a2191e8923f6b40e70cd072c8
MaryamJami1 <maryamchouhan373@gmail.com>
2024-10-24 02:27:19 +0500

    hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import styles from "./hero.module.css"
+import Image from 'next/image'
 import Btn from '../Button/Btn'
 export default function hero() {
   return (
     <header>
       <div className={styles.bg}>
-<div className="text">
-  <h1></h1>
-  <p></p>
-  <Btn text={'Get Started Now'}/>
-</div>
-<div className="img">
-
-</div>
+        <div className={styles.text}>
+          <h1>The Technology <br/> Beyond Artificial <br /> Intelligent</h1>
+          <p>We provide many features that you can use cheaply and easily. Try it <br/> now and get an interesting promo</p>
+         <Btn text={'Get Started Now'}/>
+        </div>
+        <div>
+          <Image
+            src={'/hero/hero_bg_10_1.png'}
+            alt={"robot"}
+            height={640}
+            width={500}
+          />
+        </div>
       </div>
     </header>
   )
