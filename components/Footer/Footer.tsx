c1a46a2f400669e46b0e18da910efd9120380229
MaryamJami1 <maryamchouhan373@gmail.com>
2024-10-27 19:14:27 +0500

    footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import styles from './footer.module.css'
 import { FaLinkedinIn } from "react-icons/fa";
-import { FaFacebookF } from "react-icons/fa";
+import { RiFacebookFill } from "react-icons/ri";
+
 import { FaTwitter } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa6";
+import Link from 'next/link';
 
 
 
@@ -13,13 +15,13 @@ export default function Footer() {
             <div className={styles.text}>
                 <p>Copyright 2024 <span> WebTeck </span>. All Rights Reserved.</p>
             </div>
-            <div className={styles.icon}>
-                <FaFacebookF />
+            <div className={styles.icons}>
+          <Link href={'https://www.facebook.com/'} className={styles.link}><RiFacebookFill className={styles.icon}/></Link>  
 
-                <FaLinkedinIn />
+          <Link href={'https://www.linkedin.com/signup'} className={styles.link}> <FaLinkedinIn className={styles.icon}/></Link> 
 
-                <FaTwitter />
-                <FaYoutube />
+          <Link href={'https://twitter.com/login'} className={styles.link}> <FaTwitter className={styles.icon}/></Link> 
+          <Link href={'https://www.youtube.com/'} className={styles.link}> <FaYoutube className={styles.icon} /></Link> 
 
 
 
