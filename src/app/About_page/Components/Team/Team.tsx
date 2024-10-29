d0445bff142f7808b021c97c6351916ec85e048b
MaryamJami1 <maryamchouhan373@gmail.com>
2024-10-29 23:01:48 +0500

    responsive

--- src/app/About_page/Components/Team/Team.tsx
+++ src/app/About_page/Components/Team/Team.tsx
@@ -5,13 +5,13 @@ export default function Team() {
     return (
         <div>
             <div className={styles.team_header}>
-            <h2 className={styles.linebefore}>Our Team</h2>
-                <h2>See Our Skilled Expert <span className={styles.span}>Team</span></h2>
+                <h2 className={styles.linebefore}>Our Team</h2>
+                <h2 className={styles.team}>See Our Skilled Expert <span className={styles.span}>Team</span></h2>
             </div>
 
             <div className={styles.team_container}>
                 <div className={styles.team_member}>
-                    <div className={styles.team_image} style={{ backgroundImage: "url('team/team_1_4.jpg')"}}></div>
+                    <div className={styles.team_image} style={{ backgroundImage: "url('team/team_1_4.jpg')" }}></div>
                     <div className={styles.team_info}>
                         <h4>Ann Yong</h4>
                         <p>Marketing Consultant</p>
@@ -25,7 +25,7 @@ export default function Team() {
 
 
                 <div className={styles.team_member}>
-                    <div className={styles.team_image} style={{ backgroundImage:  "url('team/team_1_3.jpg')"}}></div>
+                    <div className={styles.team_image} style={{ backgroundImage: "url('team/team_1_3.jpg')" }}></div>
                     <div className={styles.team_info}>
                         <h4>Ann Yong</h4>
                         <p>Marketing Consultant</p>
@@ -64,7 +64,7 @@ export default function Team() {
                         <li><a href="https://www.google.co.uk/">Google</a></li>
                     </ul>
                 </div>
-             
+
             </div>
         </div>
 
