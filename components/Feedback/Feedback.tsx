c60dba375d1fc7a5f81487540525e221361a9b64
MaryamJami1 <maryamchouhan373@gmail.com>
2024-10-30 23:43:00 +0500

    complete

--- components/Feedback/Feedback.tsx
+++ components/Feedback/Feedback.tsx
@@ -17,9 +17,7 @@ export default function feedback() {
                         <div className={styles.img}>
                             <div>
                                 <Image src={'/feedback/testi_img_2.png'} alt={'logo'} width={120} height={50} />
-                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima, explicabo?Lorem <br />ipsum dolor sit amet consectetur adipisicing elit.  maxime? <br />ipsum dolor sit amet consectetur adipisicing Lorem, ipsum. Lorem ipsum d elit.  <br />ipsum dolor sit amet consectetur adipisicing elit. </p>
-
-                            </div>
+                                <p>Working with this firm has been an exceptional experience. <br /> Their professionalism and attention to detail truly stand out. <br /> They listened to my needs and provided tailored solutions. <br /> The communication throughout the process was top-notch. <br /> I would highly recommend their services to anyone <br /> seeking quality support.</p>                            </div>
 
                             <div className={styles.div2}>
                                 <Image src={'/feedback/testi_2_2.png'} alt={'logo'} width={50} height={50} />
@@ -36,7 +34,7 @@ export default function feedback() {
                         <div className={styles.img}>
                             <div>
                                 <Image src={'/feedback/testi_img_2.png'} alt={'logo'} width={120} height={50} />
-                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima, explicabo?Lorem <br />ipsum dolor sit amet consectetur adipisicing elit.  maxime? <br />ipsum dolor sit amet consectetur adipisicing Lorem, ipsum. Lorem ipsum d elit.  <br />ipsum dolor sit amet consectetur adipisicing elit. </p>
+                                <p>Working with this firm has been an exceptional experience. <br /> Their professionalism and attention to detail truly stand out. <br /> They listened to my needs and provided tailored solutions. <br /> The communication throughout the process was top-notch. <br /> I would highly recommend their services to anyone <br /> seeking quality support.</p> 
 
                             </div>
 
@@ -55,7 +53,7 @@ export default function feedback() {
                         <div className={styles.img}>
                             <div>
                                 <Image src={'/feedback/testi_img_2.png'} alt={'logo'} width={120} height={50} />
-                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima, explicabo?Lorem <br />ipsum dolor sit amet consectetur adipisicing elit.  maxime? <br />ipsum dolor sit amet consectetur adipisicing Lorem, ipsum. Lorem ipsum d elit.  <br />ipsum dolor sit amet consectetur adipisicing elit. </p>
+                                <p>Working with this firm has been an exceptional experience. <br /> Their professionalism and attention to detail truly stand out. <br /> They listened to my needs and provided tailored solutions. <br /> The communication throughout the process was top-notch. <br /> I would highly recommend their services to anyone <br /> seeking quality support.</p>  
 
                             </div>
 
